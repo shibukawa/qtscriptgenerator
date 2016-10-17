3b6ebbb7828fcb9114e65d11e2866b6500800ead
Philipp A. Baer <github@ph.baer.one>
2016-10-17 11:16:48 +0200

    Compatibility improvements for Qt 5.7.0
    Add include path for QtWidgets framework headers. Fix signature or assignment operator which fixes C2582 with compiling with MSVC 2010/2012.

--- generator/parser/rpp/pp-iterator.h
+++ generator/parser/rpp/pp-iterator.h
@@ -84,7 +84,7 @@ public:
   inline pp_output_iterator &operator ++ () { return *this; }
   inline pp_output_iterator operator ++ (int) { return *this; }
   // Fix C2582 in MSVC2010/2012: http://stackoverflow.com/questions/2791525/stl-operator-behavior-change-with-visual-studio-2010
-  inline pp_output_iterator &operator=(const pp_output_iterator<_Container>& __v)
+  inline pp_output_iterator &operator=(const typename pp_output_iterator<_Container>& __v)
   {
     _M_result = __v._M_result;
     return *this;
